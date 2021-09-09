8ae005e63f963a717c0430e7ecf4f99da1b00d4c
J. Natalia Ramirez <jnataliaramirez@gmail.com>
2021-09-09 20:15:06 +0200

    Do fetch with api tvmaze

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-console.log('>> Ready :)');
+// * Variables del mundo html a js 
+const searchInput = document.querySelector('.js_search_input');
+const submit = document.querySelector('.js_button');
+const searchSection = document.querySelector('.js_search_section');
+
+// * Variables globales
+let dataSeries = [];
+
+
+// * Hacer petición al servidor - Fetch
+fetch (`http://api.tvmaze.com/search/shows?q=girls`) 
+    .then((response) => response.json())
+    .then((data) => {
+        for (let i = 0; i < data.length; i++) {
+            dataSeries = data[i].show;
+            console.log(dataSeries);
+            
+            renderSearch();
+        }
+    });
+

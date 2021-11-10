5495e02d6e6b096d22ef2e03dd69e939a3131c3b
Natalia Ramirez Parra <jnataliaramirez@gmail.com>
2021-11-10 18:19:17 +0100

    Add request to Api, for charge series when initiality the page

--- src/js/02_fecth.js
+++ src/js/02_fecth.js
@@ -1,32 +1,50 @@
 // *** Recoger input de la usuaria y hacer petición al servidor
 
-// * Funcion manejadora
+// ** Hacer petición al fetch cuando cargue la pág.para traer los 1º resultados
+
+fetch(`https://api.tvmaze.com/shows?page=1`)
+  .then((response) => response.json())
+  .then((data) => {
+    // * Devuelve solo 18 elementos del array. 
+    let newData = data.slice(-18);
+
+    // * Tomar la info de newData para añadir en un nuevo array dataShows
+    const addDataShows = newData.map(aData => dataShows.push(aData) )
+
+    // * Pintar en pantalla los datos recibidos por el servidor
+    renderSearch();
+
+    // * Guardar los datos que ha dado el servidor en localStorage
+    setInLocalStorage();
+  });
+
+// **Petición cuando se solicite buscar una serie
+
+// * Funcion manejadora 
 function handlerSearch(event) {
-    let valueInput = searchInput.value;
+  let valueInput = searchInput.value;
 
-    // *** Hacer petición al servidor - Fetch
-    fetch(`http://api.tvmaze.com/search/shows?q=${valueInput}`)
-        .then((response) => response.json())
-        .then((data) => {
+  // *** Hacer petición al servidor - Fetch
+  fetch(`http://api.tvmaze.com/search/shows?q=${valueInput}`)
+    .then((response) => response.json())
+    .then((data) => {
+      // * Este arreglo esta vacio para cuando se vuelva hacer un fetch la ventana se vacie
+      dataShows = [];
 
-            // * Este arreglo esta vacio para cuando se vuelva hacer un fetch la ventana se vacie
-            dataShows = [];
+      // * Tomar la info de data.show para añadir en un nuevo array dataShows**
+      for (const eachEl of data) {
+        let eachShow = eachEl.show;
+        dataShows.push(eachShow);
+      }
 
-            // * Tomar la info de data.show para añadir en un nuevo array dataShows**
-            for (const eachEl of data) {
-                let eachShow = eachEl.show;
-                dataShows.push(eachShow);
-            }
+      // * Pintar en pantalla los datos recibidos por el servidor
+      renderSearch();
 
-            // * Pintar en pantalla los datos recibidos por el servidor
-            renderSearch();
+      // * Guardar los datos que ha dado el servidor en localStorage
+      setInLocalStorage();
+    });
 
-            // * Guardar los datos que ha dado el servidor en localStorage
-            setInLocalStorage();
-
-        });
-
-    event.preventDefault();
+  event.preventDefault();
 }
 
 // * Listener
